7e04c457ba5906e6a3c9bf03b1bb89ab385746ba
Yichuan Ma <yichuanma@yichuans-mbp.lan>
2022-07-05 17:51:43 -0400

    putting my data into aws

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 A JSON API for the FooDB database.
 
 Data source: www.foodb.ca. Data was last updated on 7 April 2020.
+
+The data for this API consists of food data from FooDB, except that there are compound contents for each food item in
+this data. This data was generated with my Jupyter notebook, but it took a while to generate (~2 hours), so I have it
+saved and stored in an AWS S3 bucket.
+
+Download my modified data here: https://foodb-food-compound-contents.s3.amazonaws.com/FoodCompound.json.zip
--- parser.py
+++ parser.py
@@ -60,52 +60,15 @@ def extract_contents(data_dir=''):
     return pd.DataFrame(np.array(data_list), columns=columns)
 
 def load_data(data_folder):
-    content = extract_contents(data_folder)
-    compound = extract_json('Compound.json', data_folder)
-    food = extract_json('Food.json', data_folder)
-    food_ids = food['id'].to_list()
-    food_data = []
-    for food_id in food_ids:
-        compound_ids = pd.unique(content[content['food_id'] == food_id]['source_id'])
-        compound_list = []
-        for cid in compound_ids:
-            contents_dict = content[(content['food_id'] == food_id) &
-                                    (content['source_id'] == cid)][['orig_content', 'orig_unit', 'citation']].to_dict()
-            content_nums = np.array(list(map(lambda x: float(x), contents_dict['orig_content'].values())))
-            avg = np.mean(content_nums)
-            if math.isclose(avg, 0.0): continue
-            extras = {
-                'orig_contents': {
-                    'min': np.min(content_nums),
-                    'max': np.max(content_nums),
-                    'avg': avg,
-                    'unit': list(set(contents_dict['orig_unit'].values()))[0],
-                },
-                'reference': list(set(contents_dict['citation'].values()))[0]
-            }
-            try:
-                compound_entry = list(compound[compound['id'] == cid].to_dict('index').values())[0]
-                compound_entry.update(extras)
-                compound_entry['_id'] = compound_entry.pop('public_id', None)
-                compound_entry.pop('id', None)
-                compound_entry.pop('moldb_smiles', None)
-                compound_entry.pop('moldb_inchi', None)
-                compound_entry.pop('moldb_mono_mass', None)
-                compound_entry.pop('moldb_iupac', None)
-                compound_entry['inchikey'] = compound_entry.pop('moldb_inchikey', None)
-                compound_entry = dict_sweep(compound_entry, vals=[np.nan, None])
-                compound_list.append(compound_entry)
-            except IndexError:
-                continue
+    infile = os.path.join(data_folder, 'FoodCompound.json')
+    assert os.path.exists(infile)
+    docs = []
+    with open(infile) as f:
+        for line in f:
+            doc = json.loads(line)
+            docs.append(doc)
 
-        food_entry = list(food[food['id'] == food_id].to_dict('index').values())[0]
-        food_entry['compounds'] = compound_list
-        food_entry['_id'] = food_entry.pop('public_id', None)
-        food_entry.pop('id', None)
-        food_entry = dict_sweep(food_entry, vals=[np.nan, None])
-        food_data.append(food_entry)
-
-    for doc in food_data:
+    for doc in docs:
         yield doc
 
 
